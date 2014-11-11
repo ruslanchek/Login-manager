9801c65326bcdf13bb9216cd1b9c77f8480e1e21
Ruslanchek <rshashkov@icloud.com>
2014-11-11 23:23:25 +0400

    Grunt!

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    paths: ["less"]
+                },
+                files: {
+                    "css/project.css": "less/project.less"
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['less/*.less'],
+                tasks: ['less'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['less', 'watch']);
+};
--- js/min/login-min.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(".login header .icon").hover(function(){$(".login header").css({background:$(this).data("color")})},function(){$(".login header").css({background:"#37404C"})}),$("#go-restore-password").on("click",function(){$(".flip-container").addClass("rotate")}),$("#go-login").on("click",function(){$(".flip-container").removeClass("rotate")})});
